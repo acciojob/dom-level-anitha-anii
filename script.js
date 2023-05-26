96592aed5ce5bb261cdd068872722721ee7b905d
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-26 16:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+// script.js
+window.onload = function() {
+  var element = document.getElementById("level");
+  var level = getDOMLevel(element);
+  alert("The level of the element is: " + level);
+};
+
+function getDOMLevel(element) {
+  var level = 1;
+  var currentNode = element;
+
+  while (currentNode.parentNode) {
+    level++;
+    currentNode = currentNode.parentNode;
+  }
+
+  return level;
+}
